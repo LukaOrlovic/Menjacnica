549d2fdc6c58b23c60243a3776600b3f84d9c43e
LukaOrlovic <orlovicluka@hotmail.com>
2016-03-23 22:34:37 +0100

    Napravljena klasa Implementacija u kojoj su implementirane prazne metode

---
+++ src/Menjacnica/Implementacija.java
@@ -0,0 +1,27 @@
+package Menjacnica;
+
+import java.util.GregorianCalendar;
+
+import specifikacija.Specifikacija;
+
+public class Implementacija implements Specifikacija {
+
+	@Override
+	public void dodajKursValute(Kursevi kurs, GregorianCalendar naTajDatum) {
+		
+
+	}
+
+	@Override
+	public void brisanjeKursaValute(Kursevi kurs, GregorianCalendar naOvajDatum) {
+		
+
+	}
+
+	@Override
+	public Kursevi kursValuteNaOdredjeniDan(GregorianCalendar zaOvajDatum) {
+		
+		return null;
+	}
+
+}
