31ff5c60fc905a1157530e4b636072835719f83a
LukaOrlovic <orlovicluka@hotmail.com>
2016-03-24 23:31:22 +0100

    Implementirana metoda dodavanja kursa valute u klasi Implementacija.

--- src/Menjacnica/Implementacija.java
+++ src/Menjacnica/Implementacija.java
@@ -4,12 +4,26 @@ import java.util.GregorianCalendar;
 
 import specifikacija.Specifikacija;
 
-public class Implementacija implements Specifikacija {
+public class Implementacija extends Menjacnica implements Specifikacija {
 
 	@Override
 	public void dodajKursValute(Kursevi kurs, GregorianCalendar naTajDatum) {
 		
+		int i = 0;
+		
+		while(this.valute.get(i) != null){
 
+			int j = 0;
+			
+			while(this.valute.get(i).getKurs().get(j) != null){
+				
+				if((this.valute.get(i).getKurs().get(j).getDatum()) == naTajDatum){
+					this.valute.get(i).getKurs().set(j, kurs);
+	     		}
+				j++;
+			}
+			i++;		
+		}
 	}
 
 	@Override
