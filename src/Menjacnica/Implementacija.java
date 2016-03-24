29a070b0dc11de08360ff4d979a85d1f1b8e135a
LukaOrlovic <orlovicluka@hotmail.com>
2016-03-24 23:11:36 +0100

    Implementirane metode interfejsa Specifikacija u klasi Implementacija.

--- src/Menjacnica/Implementacija.java
+++ src/Menjacnica/Implementacija.java
@@ -4,23 +4,47 @@ import java.util.GregorianCalendar;
 
 import specifikacija.Specifikacija;
 
-public class Implementacija implements Specifikacija {
+public class Implementacija extends Menjacnica implements Specifikacija {
 
 	@Override
 	public void dodajKursValute(Kursevi kurs, GregorianCalendar naTajDatum) {
-		
+
+		for (int i = 0; i < this.valute.size(); i++) {  
+			for (int j = 0; j < this.valute.get(i).getKurs().size(); j++) {
+
+				if((this.valute.get(i).getKurs().get(j).getDatum()) == naTajDatum){
+					this.valute.get(i).getKurs().add(kurs);
+				}	
+			}
+		}
 
 	}
 
 	@Override
 	public void brisanjeKursaValute(Kursevi kurs, GregorianCalendar naOvajDatum) {
 		
+		for (int i = 0; i < this.valute.size(); i++) {  
+			for (int j = 0; j < this.valute.get(i).getKurs().size(); j++) {
 
+				if((this.valute.get(i).getKurs().get(j).getDatum()) == naOvajDatum){
+					this.valute.get(i).getKurs().remove(kurs);
+				}	
+			}
+		}
+		
 	}
 
 	@Override
 	public Kursevi kursValuteNaOdredjeniDan(GregorianCalendar zaOvajDatum) {
-		
+
+		for (int i = 0; i < this.valute.size(); i++) {  
+			for (int j = 0; j < this.valute.get(i).getKurs().size(); j++) {
+
+				if((this.valute.get(i).getKurs().get(j).getDatum()) == zaOvajDatum){
+					return this.valute.get(i).getKurs().get(j);
+				}	
+			}
+		}
 		return null;
 	}
 
